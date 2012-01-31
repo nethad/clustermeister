1e367a41ebc91fab57a9f3937060f40d54082371
Daniel Spicar <donnervogelistaken@gmail.com>
2012-01-31 16:52:23 +0100

    added dummy java files, so the directory structure is checked out

---
+++ api/src/main/java/ch/uzh/ifi/ddis/clustermeister/api/ClustermeisterAPI.java
@@ -0,0 +1,10 @@
+
+package ch.uzh.ifi.ddis.clustermeister.api;
+
+/**
+ *
+ * @author daniel
+ */
+public class ClustermeisterAPI {
+	
+}
---
+++ provisioning/src/main/java/ch/uzh/ifi/ddis/clustermeister/provisioning/ClustermeisterProvisioning.java
@@ -0,0 +1,10 @@
+
+package ch.uzh.ifi.ddis.clustermeister.provisioning;
+
+/**
+ *
+ * @author daniel
+ */
+public class ClustermeisterProvisioning {
+	
+}
