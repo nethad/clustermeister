d49a31870d460d9767659cbc8841c0bcdadd8587
Thomas R <nethad@gmx.net>
2012-02-27 13:41:36 +0100

    remove local driver files

--- provisioning/src/main/resources/com/github/nethad/clustermeister/provisioning/torque/jppf-driver-local/startDriver.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-$1 -cp config:lib/* -Xmx32m -Djppf.config=jppf-driver.properties -Dlog4j.configuration=log4j-driver.properties -Djava.util.logging.config.file=config/logging-driver.properties org.jppf.server.DriverLauncher
