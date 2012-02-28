20fde23178dfd5d38e2685731c96cbacf733b735
Daniel Spicar <donnervogelistaken@gmail.com>
2012-02-28 10:55:39 +0100

    Added non functional implementations of Node, NodeConfiguration and NodeManager for TORQUE.

---
+++ provisioning/src/main/java/com/github/nethad/clustermeister/provisioning/torque/TorqueNode.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012 The Clustermeister Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.nethad.clustermeister.provisioning.torque;
+
+import com.github.nethad.clustermeister.api.Node;
+import com.github.nethad.clustermeister.api.NodeType;
+import java.util.Set;
+
+/**
+ * TODO: lightweight class with information about created nodes.
+ * - should not perform any web requests/long running operations
+ * - a node is a JPPF node or a JPPF driver, not directly related to TOQUE nodes. 
+ * - Multiple JPPF nodes may run on the same TORQUE node.
+ *
+ * @author daniel
+ */
+public class TorqueNode implements Node {
+
+	@Override
+	public String getID() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public NodeType getType() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Set<String> getPublicAddresses() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Set<String> getPrivateAddresses() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+	
+}
---
+++ provisioning/src/main/java/com/github/nethad/clustermeister/provisioning/torque/TorqueNodeManager.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2012 The Clustermeister Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.nethad.clustermeister.provisioning.torque;
+
+/**
+ * TODO: 
+ * - functionality like add(deploy)/remove JPPD nodes.
+ * - starting/deploying drivers
+ * - this is not yet to be directly used by the user but by a higher layer of abstraction (to be defined)
+ * - this will likely need to implement a yet to be defined interface together with AmazonNodeManager
+ *
+ * @author daniel
+ */
+public class TorqueNodeManager {
+	
+}
