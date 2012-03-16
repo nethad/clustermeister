e73b53af94a08ba6f1fc027115f1539098b53ab6
Thomas R <nethad@gmx.net>
2012-03-16 17:52:15 +0100

    delete old resources

--- provisioning/src/main/resources/com/github/nethad/clustermeister/provisioning/torque/jppf-node/startNode.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-java -cp config:lib/* -Xms64m -Xmx512m -Djppf.config=$1 -Dlog4j.configuration=log4j-node.properties -Djava.util.logging.config.file=config/logging-node.properties org.jppf.node.NodeLauncher
