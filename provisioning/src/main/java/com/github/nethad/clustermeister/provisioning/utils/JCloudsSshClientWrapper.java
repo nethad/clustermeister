27f76666ef80d7b834236a2e7edd544dfbd4bcf2
Daniel Spicar <donnervogelistaken@gmail.com>
2012-05-01 17:11:17 +0200

    extended implementation of JCloudsSshClientWrapper.

--- provisioning/src/main/java/com/github/nethad/clustermeister/provisioning/utils/JCloudsSshClientWrapper.java
+++ provisioning/src/main/java/com/github/nethad/clustermeister/provisioning/utils/JCloudsSshClientWrapper.java
@@ -22,18 +22,70 @@ import org.jclouds.io.Payloads;
 import org.jclouds.ssh.SshClient;
 
 /**
- * only for internal use.
+ * Wraps a jClouds {@link SshClient} with a {@link SSHClient}.
+ * 
+ * Note: intended for internal use only.
  *
  * @author daniel
  */
 public class JCloudsSshClientWrapper implements SSHClient {
     
     private final SshClient sshClient;
+    private final int port;
 
-    public JCloudsSshClientWrapper(SshClient sshClient) {
+    /**
+     * A new JCloudsSshClientWrapper.
+     * 
+     * @param sshClient the wrapped jClouds SshClient instance.
+     * @param port 
+     *      The SSH port, this is not used to connect to the specified port 
+     *      but only to support querying it. The correct port must be configured 
+     *      on the wrapped SshClient instance.
+     */
+    public JCloudsSshClientWrapper(SshClient sshClient, int port) {
         this.sshClient = sshClient;
+        this.port = port;
+    }
+    
+    /**
+     * A new JCloudsSshClientWrapper with SSH port 22.
+     * 
+     * @param sshClient the wrapped jClouds SshClient instance.
+     */
+    public JCloudsSshClientWrapper(SshClient sshClient) {
+        this(sshClient, 22);
     }
 
+    /**
+     * Get the host name to connect to.
+     * 
+     * @return the host name.
+     */
+    public String getHost() {
+        return sshClient.getHostAddress();
+    }
+
+    /**
+     * Get the user name to use.
+     * 
+     * @return the SSH user name.
+     */
+    public String getUserName() {
+        return sshClient.getUsername();
+    }
+
+    /**
+     * Get the port to connect to.
+     * 
+     * This value may not be correct. It is always the value set when this class 
+     * was instantiated, not the actual value used by the wrapped client.
+     * 
+     * @return the port.
+     */
+    public int getPort() {
+        return port;
+    }
+    
     @Override
     public void connect(String userName, String host) throws SSHClientException {
         sshClient.connect();
@@ -63,6 +115,7 @@ public class JCloudsSshClientWrapper implements SSHClient {
 
     @Override
     public void sftpUpload(String srcPath, String destPath) throws SSHClientException {
+        //TODO: does not recursively upload directories
         sshClient.put(destPath, Payloads.newFilePayload(new File(srcPath)));
     }
 
@@ -76,13 +129,24 @@ public class JCloudsSshClientWrapper implements SSHClient {
         sshClient.disconnect();
     }
 
+    /**
+     * This method does nothing.
+     * 
+     * @param privateKeyPath not used.
+     * @throws SSHClientException never happens 
+     */
     @Override
     public void setPrivateKey(String privateKeyPath) throws SSHClientException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //nop
     }
 
+    /**
+     * This method is not supported by the wrapper.
+     * 
+     * @return always throws an UnsupportedOperationException.
+     */
     @Override
     public boolean isConnected() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("This operation can not be supported.");
     }
 }
