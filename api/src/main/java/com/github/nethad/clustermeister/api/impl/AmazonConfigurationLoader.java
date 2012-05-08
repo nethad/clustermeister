c0290d72233011e5fc9424710f61640b7f6799fa
Daniel Spicar <donnervogelistaken@gmail.com>
2012-05-08 15:03:19 +0200

    Added javadoc and tests for AmazonConfigurationLoader.

--- api/src/main/java/com/github/nethad/clustermeister/api/impl/AmazonConfigurationLoader.java
+++ api/src/main/java/com/github/nethad/clustermeister/api/impl/AmazonConfigurationLoader.java
@@ -26,36 +26,81 @@ import java.util.Map;
 import org.apache.commons.configuration.Configuration;
 
 /**
+ * A utility to help parse Amazon specific configuration values.
  *
  * @author daniel
  */
 public class AmazonConfigurationLoader {
-    
+    /**
+     * AWS Access key ID.
+     */
     public static final String ACCESS_KEY_ID = "amazon.access_key_id";
     
+    /**
+     * AWS secret key.
+     */
     public static final String SECRET_KEY = "amazon.secret_key";
     
+    /**
+     * Amazon key pair configuration.
+     */
     public static final String KEYPAIRS = "amazon.keypairs";
-    static final String PRIVATE_KEY = "private_key";
-    static final String PUBLIC_KEY = "public_key";
-    static final String USER = "user";
     
+    /**
+     * Private key configuration.
+     */
+    public static final String PRIVATE_KEY = "private_key";
+    
+    /**
+     * Private key configuration.
+     */
+    public static final String PUBLIC_KEY = "public_key";
+    
+    /**
+     * User name configuration.
+     */
+    public static final String USER = "user";
+    
+    /**
+     * The configuration.
+     */
     final Configuration configuration;
 
+    /**
+     * Create a new Configuration Loader with a specific configuration.
+     * 
+     * @param configuration the configuration.
+     */
     public AmazonConfigurationLoader(Configuration configuration) {
         this.configuration = configuration;
     }
     
+    /**
+     * Returns the configured AWS Access Key.
+     * 
+     * @return the access key.
+     */
     public String getAccessKeyId() {
         return checkNotNull(configuration.getString(ACCESS_KEY_ID, null), 
                 "No AWS access key ID configured.").trim();
     }
     
+    /**
+     * Returns the configured Secret Key.
+     * 
+     * @return the secret key.
+     */
     public String getSecretKey() {
         return checkNotNull(configuration.getString(SECRET_KEY, null), 
                 "No AWS secret key configured.").trim();
     }
     
+    /**
+     * Returns a map containing a mapping of configured credentials names to 
+     * {@link Credentials} instances..
+     * 
+     * @return the configured credentials.
+     */
     public Map<String, Credentials> getConfiguredCredentials() {
         List<Object> keypairList = configuration.getList(KEYPAIRS);
         Map<String, Map<String, String>> keypairSpecifications = 
@@ -66,26 +111,18 @@ public class AmazonConfigurationLoader {
         for (Map.Entry<String, Map<String, String>> entry : keypairSpecifications.entrySet()) {
             String keyPairName = entry.getKey();
             Map<String, String> keyPairValues = entry.getValue();
-            checkArgument(keyPairValues.containsKey(USER), 
-                    "No key '%s' found for keypair %s.", USER, keyPairName);
-            String user = checkNotNull(keyPairValues.get(USER), 
-                    "No value for key '%s' found for keypair %s.", USER, 
-                    keyPairName).trim();
-            checkArgument(keyPairValues.containsKey(PRIVATE_KEY), 
-                    "No key '%s' found for keypair %s.", PRIVATE_KEY, keyPairName);
-            String privateKeyPath = checkNotNull(keyPairValues.get(PRIVATE_KEY), 
-                    "No value for key '%s' found for keypair %s.", PRIVATE_KEY, 
-                    keyPairName).trim();
-            File privateKey = new File(privateKeyPath);
-            checkArgument(privateKey.isFile() && privateKey.canRead(), 
-                    "Private key for keypair %s can not be read from.", keyPairName);
+            String user = getCheckedConfigValue(USER, keyPairValues, "keypair", 
+                    keyPairName);
+            String privateKeyPath = getCheckedConfigValue(PRIVATE_KEY, 
+                    keyPairValues, "keypair", keyPairName);
+            File privateKey = getCheckedFile(privateKeyPath, PRIVATE_KEY, 
+                    "keypair", keyPairName);
             
             String publicKeyPath = keyPairValues.get(PUBLIC_KEY);
             if(publicKeyPath != null) {
-                File publicKey = new File(publicKeyPath.trim());
-                checkArgument(publicKey.isFile() && publicKey.canRead(), 
-                        "Public key for keypair %s can not be read from.", keyPairName);
-                credentials.put(keyPairName, 
+                File publicKey = getCheckedFile(publicKeyPath, PUBLIC_KEY, 
+                        "keypair", keyPairName);
+                credentials.put(keyPairName,
                         new KeyPairCredentials(user, privateKey, publicKey));
             } else {
                 credentials.put(keyPairName, 
@@ -97,6 +134,22 @@ public class AmazonConfigurationLoader {
         return credentials;
     }
     
+    private File getCheckedFile(String path, String key, String listObjectCategory, String listObjectName) {
+        File file = new File(path);
+        checkArgument(file.isFile() && file.canRead(),
+                "%s for %s %s can not be read from.", key, listObjectCategory, listObjectName);
+        return file;
+    }
+    
+    private String getCheckedConfigValue(String key, Map<String, String> configMap, 
+            String listObjectCategory, String listObjectName) {
+        checkArgument(configMap.containsKey(key),
+                "No key '%s' found for %s %s.", key, listObjectCategory, listObjectName);
+        String value = checkNotNull(configMap.get(key),
+                "No value for key '%s' found for %s %s.", key, listObjectCategory, 
+                listObjectName).trim();
+        return value;
+    }
     
     private Map<String, Map<String, String>> reduceObjectList(List<Object> list, 
             String errorMessage) {
