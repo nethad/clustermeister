3b7605b25ef8a96cba8a1e5def2d767a2b4e8de4
Daniel Spicar <donnervogelistaken@gmail.com>
2012-05-25 11:43:23 +0200

    Fixed logging for local nodes.

--- provisioning/src/main/java/com/github/nethad/clustermeister/provisioning/local/JPPFLocalNode.java
+++ provisioning/src/main/java/com/github/nethad/clustermeister/provisioning/local/JPPFLocalNode.java
@@ -15,18 +15,23 @@
  */
 package com.github.nethad.clustermeister.provisioning.local;
 
-import com.github.nethad.clustermeister.api.JPPFConstants;
-import com.github.nethad.clustermeister.provisioning.jppf.JPPFConfiguratedComponentFactory;
-import com.github.nethad.clustermeister.provisioning.jppf.JPPFManagementByJobsClient;
-import com.github.nethad.clustermeister.provisioning.jppf.JPPFNodeConfiguration;
-import com.google.common.io.Files;
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collection;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.github.nethad.clustermeister.api.JPPFConstants;
+import com.github.nethad.clustermeister.provisioning.jppf.JPPFNodeConfiguration;
+import com.google.common.io.Files;
 
 /**
  *
@@ -35,12 +40,6 @@ import org.slf4j.LoggerFactory;
 public class JPPFLocalNode {
     private final Logger logger = LoggerFactory.getLogger(JPPFLocalNode.class);
     
-    private Process nodeProcess;
-//    private NodeProcessOutputter nodeProcessOutputterStdOut;
-    private NodeProcessOutputterToFile nodeProcessOutputterStdOutToFile;
-//    private NodeProcessOutputter nodeProcessOutputterStdErr;
-    private NodeProcessOutputterToFile nodeProcessOutputterStdErrToFile;
-//    private File tempDir;
     private File targetDir;
     
     private int managementPort = 12001;
@@ -56,12 +55,6 @@ public class JPPFLocalNode {
         libDir = new File(targetDir, "jppf-node/lib/");
         preloadLibraries(artifactsToPreload);
     }
-    
-//    public void prepare() {
-//        unpackNodeZip();
-//        libDir = new File(targetDir, "jppf-node/lib/");
-////        preloadLibraries();
-//    }
     
     private void preloadLibraries(Collection<File> artifactsToPreload) {
         for (File artifact : artifactsToPreload) {
@@ -121,40 +114,16 @@ public class JPPFLocalNode {
             final String command = String.format("%s %s %s %s %s",
                     "./" + startNodeScript.getName(), 
                     currentNodeConfig,
-                    "true", "false", jvmOptions);
+                    "false", "false", jvmOptions);
             logger.info("Start node with {}", command);
-            nodeProcess = Runtime.getRuntime().exec(command, new String[]{}, startNodeScript.getParentFile());
-//            Process exec = Runtime.getRuntime().exec(command);
-            InputStream stdOutInputStream = nodeProcess.getInputStream();
-            InputStream stdErrInputStream = nodeProcess.getErrorStream();
-            //            CharStreams.class;
-            BufferedReader stdOutReader = new BufferedReader(new InputStreamReader(stdOutInputStream));
-            BufferedReader stdErrReader = new BufferedReader(new InputStreamReader(stdErrInputStream));
-//            nodeProcessOutputterStdOut = new NodeProcessOutputter(stdOutReader, "STDOUT");
-            nodeProcessOutputterStdOutToFile = new NodeProcessOutputterToFile(stdOutReader, "STDOUT", createLogFile("node.stdout"));
-            nodeProcessOutputterStdOutToFile.start();
-//            nodeProcessOutputterStdErr = new NodeProcessOutputter(stdErrReader, "STDERR");
-            nodeProcessOutputterStdErrToFile = new NodeProcessOutputterToFile(stdErrReader, "STDERR", createLogFile("node.stderr"));
-            nodeProcessOutputterStdErrToFile.start();
+            Runtime.getRuntime().exec(command, new String[]{}, startNodeScript.getParentFile());
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
     }
     
-    private File createLogFile(String name) {
-        File logFile = new File(targetDir, name + "." + currentNodeNumber);
-        try {
-            logFile.createNewFile();
-        } catch (IOException ex) {
-            log("Could not create {}", logFile.getAbsolutePath(), ex);
-        }
-        return logFile;
-    }
-    
     public void cleanupAfterShutdown() {
         try {
-            nodeProcessOutputterStdOutToFile.close();
-            nodeProcessOutputterStdErrToFile.close();
             FileUtils.deleteDirectory(targetDir);
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -209,103 +178,4 @@ public class JPPFLocalNode {
         in.close();
         out.close();
     }
-    
-    private synchronized void log(String message, String... args) {
-//        logger.info(message, args);
-    	System.out.println(String.format(message.replaceAll("\\{\\}", "%s"), (Object[])args));
-    }
-    
-    private synchronized void log(String message, String arg, Exception ex) {
-//        logger.info(message, arg, ex);
-        System.out.println(String.format(message.replaceAll("\\{\\}", "%s"), arg));
-        System.out.println("Exception: "+ex.getMessage());
-        ex.printStackTrace();
-    }
-    
-    public class NodeProcessOutputter extends Thread {
-        
-        private BufferedReader reader;
-        private final String logMarker;
-
-        private NodeProcessOutputter(BufferedReader reader, String logMarker) {
-            this.reader = reader;
-            this.logMarker = logMarker;
-        }
-
-        @Override
-        public void run() {
-            log("{} started.", getClass().getName());
-            try {
-                String nextLine;
-                while ((nextLine = reader.readLine()) != null) {
-                    log("{}: {}", logMarker, nextLine);
-                }
-            } catch (IOException ex) {
-                log("IOException while printing {}.", logMarker, ex);
-            }
-        }
-
-        public void close() {
-            try {
-                this.reader.close();
-            } catch (IOException ex) {
-                // ignore
-            }
-        }
-        
-    }
-    
-    public class NodeProcessOutputterToFile extends Thread {
-        
-        private final BufferedReader reader;
-        private final String logMarker;
-        private final FileWriter fileWriter;
-
-        private NodeProcessOutputterToFile(BufferedReader reader, String logMarker, File logToFile) throws IOException {
-            this.reader = reader;
-            this.logMarker = logMarker;
-            fileWriter = new FileWriter(logToFile);
-        }
-
-        @Override
-        public void run() {
-            logToFile("{} started.", getClass().getName());
-            try {
-                String nextLine;
-                while ((nextLine = reader.readLine()) != null) {
-                    logToFile("{}: {}", logMarker, nextLine);
-                }
-            } catch (IOException ex) {
-                log("IOException while printing {}.", logMarker, ex);
-            }
-        }
-
-        public void close() {
-            try {
-                this.reader.close();
-                this.fileWriter.close();
-            } catch (IOException ex) {
-                // ignore
-            }
-        }
-
-        private void logToFile(String message, String... args) {
-            try {
-                fileWriter.append(String.format(message.replaceAll("\\{\\}", "%s"), (Object[]) args));
-            } catch (IOException ex) {
-                log("Could not write to log file", null, ex);
-            }
-        }
-
-        private void logToFile(String message, String arg, Exception ex) {
-            try {
-                fileWriter.append(String.format(message.replaceAll("\\{\\}", "%s"), arg));
-                fileWriter.append("Exception: " + ex.getMessage());
-                ex.printStackTrace(new PrintWriter(fileWriter));
-            } catch (IOException ex1) {
-                log(message, arg, ex);
-            }
-        }
-    }
-    
 }
