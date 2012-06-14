675cbb13eea51bc03c37d139be436a60073392f4
Daniel Spicar <donnervogelistaken@gmail.com>
2012-06-14 14:44:06 +0200

    Issue #58: Moved abstract builders to builders package.

--- node-common/src/main/java/com/github/nethad/clustermeister/node/common/builders/ComponentBuilder.java
+++ node-common/src/main/java/com/github/nethad/clustermeister/node/common/builders/ComponentBuilder.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.nethad.clustermeister.node.common;
+package com.github.nethad.clustermeister.node.common.builders;
 
 /**
  * Responsible for the creation of components.
