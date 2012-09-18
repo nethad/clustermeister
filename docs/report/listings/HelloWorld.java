a3bd6545955372f23737d50597e673987edb440d
Thomas Ritter <nethad@gmx.net>
2012-09-18 15:20:23 +0200

    add another listing

--- docs/report/listings/HelloWorld.java
+++ docs/report/listings/HelloWorld.java
@@ -1,12 +1,29 @@
-import java.io.Serializable;
-import java.util.concurrent.Callable;
+import com.github.nethad.clustermeister.api.Clustermeister;
+import com.github.nethad.clustermeister.api.ExecutorNode;
+import com.github.nethad.clustermeister.api.impl.ClustermeisterFactory;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.concurrent.ExecutionException;
 
-public class HelloWorldCallable implements Callable<String>, Serializable {
-
-    @Override
-    public String call() throws Exception {
-        return "Hello world!";
-    }
+public class HelloWorld {
     
-}
+    public static void main(String... args) {
+        Clustermeister clustermeister = null;
+        try {
+            clustermeister = ClustermeisterFactory.create();
+            for (ExecutorNode executorNode : clustermeister.getAllNodes()) {
+                ListenableFuture<String> resultFuture = executorNode.execute(new HelloWorldCallable());
+                String result = resultFuture.get();
+                System.out.println("Node " + executorNode.getID() + ", result: " + result);
+            }
+        } catch (InterruptedException ex) {
+            System.err.println("Exception while waiting for result: " + ex.getMessage());
+        } catch (ExecutionException ex) {
+            System.err.println("Exception while waiting for result: " + ex.getMessage());
+        } finally {
+            if (clustermeister != null) {
+                clustermeister.shutdown();
+            }
+        }
+    }
 
+}
